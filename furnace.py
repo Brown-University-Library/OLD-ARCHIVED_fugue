4dabfecad9bfe9b909c99c0bcd4094ac7ca40056
Adam Bradley <adam_bradley@brown.edu>
2019-05-28 14:57:18 -0400

    Handling non-XML-friendly field names in csv/json.

--- furnace.py
+++ furnace.py
@@ -12,6 +12,9 @@ from datetime import datetime
 from csv import DictReader as csvreader
 import re
 
+#TODO: only needed for loading one file type--should live in that module when I move it.
+import json
+
 import yaml
 from lxml import etree as ET
 import mimetypes
@@ -53,7 +56,14 @@ def dict2xml(thing, targ = None):
     
     if dict == type(thing):
         for k, v in thing.items():    
-            newel = ET.SubElement(targ, k)
+            #TODO: Better identify bad XML tag names.
+            #TODO: Better share this code with the csv handling bit.
+            re_starts_with_digit = re.compile(r'(^[0-9])')
+            re_non_word_chars = re.compile(r'[^\w]+')
+            tagname = re_starts_with_digit.sub(r'_\1', k.lower())
+            tagname = re_non_word_chars.sub('_', tagname)
+            
+            newel = ET.SubElement(targ, tagname)
             if str == type(v):
                 newel.text = v
             elif bytes == type(v):
@@ -164,6 +174,12 @@ def handle_filesystem_datasource(ds, dsroot):
                         tagname = re_non_word_chars.sub('_', tagname)
                         newcell = ET.SubElement(xmlrow, tagname, {'columnname': k})
                         newcell.text=v
+
+        elif "application/json" == mimetype:
+            with p.open("r", encoding='utf8') as fl:
+                jdata = json.load(fl)
+                newtree = ET.Element('jsondata')
+                dict2xml(jdata, newtree)
 
         else: #Default to assuming this is XML or HTML, at least for now.
             with p.open("rb") as fl:
