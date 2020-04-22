eae2b5e6518d2176de20513f7a23f1a9a15b3b85
Adam Bradley <adam_bradley@brown.edu>
2020-04-22 12:05:01 -0400

    Adding documentation site output. Updating setup.py with author details.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name='Fugue',
+    author="Adam Bradley",
+    author_email='adam_bradley@brown.edu',
     version='0.9a',
     packages=['fugue', 
             'fugue.tools',
@@ -11,6 +13,7 @@ setup(
         "certifi>=2018.8.24",
         "Click>=7.0",
         "lxml>=4.1.0",
+        "markdown2==2.3.8",
         "pytidylib>=0.3.2",
         "PyYAML>=5.1",
         "urllib3>=1.25.3",
